b861e8cfc3efb03f57a393c8b893a9dbfb2efb26
itsAlif <macbookpro@MacBooks-MacBook-Pro.local>
2022-08-23 05:34:54 +0600

    prettier and eslint added

--- components/DragBox.tsx
+++ components/DragBox.tsx
@@ -1,14 +1,11 @@
-import styles from '../styles/Home.module.css';
+import { TFlag } from "../pages";
+import styles from "../styles/Home.module.css";
 
 type Props = {
-    onDrop: (item: any) => void;
-    flag: 
-}
+  onDrop: (item: any) => void;
+  flag: TFlag;
+};
 
-export default function DragBox({ onDrop, flag }) {
-  return <div
-  className={styles.imageBox}
->
-  {flag.name}
-</div>;
+export default function DragBox({ onDrop, flag }: Props) {
+  return <div className={styles.imageBox}>{flag.name}</div>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,61 @@
-import type { NextPage, InferGetServerSidePropsType } from "next";
-import React from "react";
-import Head from "next/head";
-import { useDrag } from "react-dnd";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import DragBox from "../components/DragBox";
+import type { InferGetServerSidePropsType } from 'next'
+import * as React from 'react'
+import Head from 'next/head'
+import { DndProvider, useDrag } from 'react-dnd'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import DragBox from '../components/DragBox'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 const Home = ({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
-    // "type" is required. It is used by the "accept" specification of drop targets.
-    type: "BOX",
-    // The collect function utilizes a "monitor" instance (see the Overview for what this is)
-    // to pull important pieces of state from the DnD system.
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  }));
+  const [droppedBoxNames, setDroppedBoxNames] = React.useState<Array<String>>(
+    [],
+  )
 
   const [currentFlags, setCurrentFlags] = React.useState(() =>
-    getMultipleRandom(data, 3)
-  );
+    getMultipleRandom(data, 3),
+  )
 
-  function shuffleArray(array: Array<Props>) {
-    let curId = array.length;
+  function shuffleArray(array: Array<TFlag>) {
+    let curId = array.length
     while (0 !== curId) {
-      let randId = Math.floor(Math.random() * curId);
-      curId -= 1;
-      let tmp = array[curId];
-      array[curId] = array[randId];
-      array[randId] = tmp;
+      let randId = Math.floor(Math.random() * curId)
+      curId -= 1
+      let tmp = array[curId]
+      array[curId] = array[randId]
+      array[randId] = tmp
     }
-    return array;
+    return array
   }
 
-  function getMultipleRandom(arr: Array<Props>, num: number) {
-    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+  function getMultipleRandom(arr: Array<TFlag>, num: number) {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random())
 
-    return shuffled.slice(0, num);
+    return shuffled.slice(0, num)
   }
 
-  function handleDrop(evt: React.DragEvent<HTMLDivElement>) {
-    console.log("hi");
-  }
+  const handleDrop = (index: number, flag: TFlag) => {}
+
+  // const handleDrop = React.useCallback(
+  //   (index: number, flag: TFlag) => {
+  //     const { name } = flag;
+  //     setDroppedBoxNames(
+  //       update(droppedBoxNames, name ? { $push: [name] } : { $push: [] })
+  //     );
+  //     setDustbins(
+  //       update(dustbins, {
+  //         [index]: {
+  //           lastDroppedItem: {
+  //             $set: item,
+  //           },
+  //         },
+  //       })
+  //     );
+  //   },
+  //   [droppedBoxNames, dustbins]
+  // );
 
   return (
     <div>
@@ -54,52 +66,54 @@ const Home = ({
       </Head>
       <main className={styles.main}>
         <h2>Drag and drop flags</h2>
-        <div className={styles.container}>
-          <div className={styles.flagsContainer}>
-            {currentFlags.map((flag, index) => (
-              <>
-                <DragBox
-                  onDrop={(flag: Props) => handleDrop(index, item)}
-                  key={flag.code}
-                  flag={flag}
-                />
-              </>
-            ))}
+        <DndProvider backend={HTML5Backend}>
+          <div className={styles.container}>
+            <div className={styles.flagsContainer}>
+              {currentFlags.map((flag, index) => (
+                <>
+                  <DragBox
+                    onDrop={(flag: TFlag) => handleDrop(index, flag)}
+                    key={flag.code}
+                    flag={flag}
+                  />
+                </>
+              ))}
+            </div>
+            <div className={styles.namesContainer}>
+              {shuffleArray(currentFlags).map((flag) => (
+                <div className={styles.nameBox} key={flag.code}>
+                  {flag.name}
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={styles.namesContainer}>
-            {shuffleArray(currentFlags).map((flag) => (
-              <div className={styles.nameBox} key={flag.code}>
-                {flag.name}
-              </div>
-            ))}
-          </div>
-        </div>
+        </DndProvider>
       </main>
     </div>
-  );
-};
+  )
+}
 
-type Props = {
-  name: string;
-  code: string;
-  capital: string;
-  region: string;
-  flag: string;
-};
+export type TFlag = {
+  name: string
+  code: string
+  capital: string
+  region: string
+  flag: string
+}
 
 export async function getServerSideProps() {
-  const res = await fetch(`https://countryflagapi.herokuapp.com/all`);
-  const data = await res.json();
+  const res = await fetch(`https://countryflagapi.herokuapp.com/all`)
+  const data = await res.json()
 
   if (!data) {
     return {
       notFound: true,
-    };
+    }
   }
 
   return {
-    props: { data : Props }, // will be passed to the page component as props
-  };
+    props: { data }, // will be passed to the page component as props
+  }
 }
 
-export default Home;
+export default Home
